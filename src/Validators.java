a57378c3d2100798859ca316ad796abfb3a6faac
Muhammad Rydwan <rydwan.dev@gmail.com>
2022-09-26 19:19:39 +0700

    Update checking method using isEmpty instead of isBlank

--- src/Validators.java
+++ src/Validators.java
@@ -1,12 +1,12 @@
 public class Validators {
    public static boolean validateNameLength(String studentName) {
-        if(studentName.isBlank()) return false;
+        if(studentName.isEmpty()) return false;
         if(studentName.length() < 1 || studentName.length() > 25) return false;
         return true;
     }
 
     public static boolean validateNIMLength(String NIM) {
-       if(NIM.isBlank()) return false;
+       if(NIM.isEmpty()) return false;
        if(NIM.length() > 10 || NIM.length() < 10) {
            return false;
        }
